3982c21a78ddbc1d842082b7dcd5675534f4dc61
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-08 06:38:40 +0530

    modularizing code

---
+++ server/controllers/urlController.js
@@ -0,0 +1,33 @@
+import urlSchema from "../models/shortUrl.model.js";
+import { nanoid } from "nanoid";
+
+export const createShortUrl = async(req,res)=>{
+    try {
+        const { url } = req.body;
+        const shortUrl = nanoid(7);
+        const newUrl = new urlSchema({
+            full_url: url,
+            short_url: shortUrl
+        }) 
+        await newUrl.save()
+        res.status(200).json({success:true, shortUrl:shortUrl});
+    } catch (error) {
+        console.error("Error redirecting:", error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
+
+export const getFullUrl = async(req,res)=>{
+    try {
+        const {shortUrl} = req.params;
+        const url = await urlSchema.findOne({short_url: shortUrl});
+        if(url){
+            res.redirect(url.full_url);
+        }else{
+            res.status(404).send({success:false, message: "Not Found"})
+        }
+    }catch(error) {
+        console.error("Error redirecting:", error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { nanoid } from "nanoid";
-import urlSchema from "../models/shortUrl.model.js";
+import { createShortUrl, getFullUrl } from "../controllers/urlController.js";
 const router = express.Router();
 
 
@@ -8,36 +7,9 @@ router.get("/", (req,res) =>{
     res.send("Hello form router");
 })
 
-router.post("/create", async(req,res) =>{
-    try {
-        const { url } = req.body;
-        const shortUrl = nanoid(7);
-        const newUrl = new urlSchema({
-            full_url: url,
-            short_url: shortUrl
-        }) 
-        await newUrl.save()
-        res.status(200).json({success:true, shortUrl:shortUrl});
-    } catch (error) {
-        console.error("Error redirecting:", error);
-        res.status(500).json({ success: false, message: "Server error" });
-    }
-})
+router.post("/create", createShortUrl)
 
-router.get("/:shortUrl", async(req,res) =>{
-    try {
-        const {shortUrl} = req.params;
-        const url = await urlSchema.findOne({short_url: shortUrl});
-        if(url){
-            res.redirect(url.full_url);
-        }else{
-            res.status(404).send({success:false, message: "Not Found"})
-        }
-    }catch(error) {
-        console.error("Error redirecting:", error);
-        res.status(500).json({ success: false, message: "Server error" });
-    }
-})
+router.get("/:shortUrl", getFullUrl)
 
 
 export default router;
