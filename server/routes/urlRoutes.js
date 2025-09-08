dfb7a424ecc4152328d1a27ddcb7407d312a1a9d
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-08 06:31:53 +0530

    /get route complete

--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -8,18 +8,36 @@ router.get("/", (req,res) =>{
     res.send("Hello form router");
 })
 
-router.post("/create", (req,res) =>{
-    const { url } = req.body;
-    const shortUrl = nanoid(7);
-    const newUrl = new urlSchema({
-        full_url: url,
-        short_url: shortUrl
-    }) 
-    newUrl.save()
-    res.status(200).json({success:true, shortUrl:shortUrl});
+router.post("/create", async(req,res) =>{
+    try {
+        const { url } = req.body;
+        const shortUrl = nanoid(7);
+        const newUrl = new urlSchema({
+            full_url: url,
+            short_url: shortUrl
+        }) 
+        await newUrl.save()
+        res.status(200).json({success:true, shortUrl:shortUrl});
+    } catch (error) {
+        console.error("Error redirecting:", error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
 })
 
-
+router.get("/:shortUrl", async(req,res) =>{
+    try {
+        const {shortUrl} = req.params;
+        const url = await urlSchema.findOne({short_url: shortUrl});
+        if(url){
+            res.redirect(url.full_url);
+        }else{
+            res.status(404).send({success:false, message: "Not Found"})
+        }
+    }catch(error) {
+        console.error("Error redirecting:", error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+})
 
 
 export default router;
