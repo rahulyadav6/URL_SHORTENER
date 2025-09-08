719bc13591f5c6f791733dff71a376e8a219c6d5
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-08 07:34:10 +0530

    modularizing code

--- server/controllers/urlController.js
+++ server/controllers/urlController.js
@@ -10,7 +10,7 @@ export const createShortUrl = async(req,res)=>{
             short_url: shortUrl
         }) 
         await newUrl.save()
-        res.status(200).json({success:true, shortUrl:shortUrl});
+        res.status(200).json({success:true, shortUrl: process.env.APP_URL+"api/url/" + shortUrl});
     } catch (error) {
         console.error("Error redirecting:", error);
         res.status(500).json({ success: false, message: "Server error" });
